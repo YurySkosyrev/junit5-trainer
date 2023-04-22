4412b325417a9866169285e0e7b17131a9679c8d
YurySkosyrev <skosyrev-87@mail.ru>
2023-04-22 22:47:22 +0300

    added some unit-test

--- src/test/java/com/dmdev/dao/UserDaoTest.java
+++ src/test/java/com/dmdev/dao/UserDaoTest.java
@@ -6,11 +6,13 @@ import com.dmdev.entity.User;
 import com.dmdev.integration.IntegrationTestBase;
 import com.dmdev.util.LocalDateFormatter;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.predicate;
 
 class UserDaoTest extends IntegrationTestBase {
 
@@ -34,7 +36,10 @@ class UserDaoTest extends IntegrationTestBase {
     }
 
     @Test
+    @DisplayName("findAll method should return 6 users after added new user")
     void usersSizeIfUserAdded() {
+
+       //Given
         User user = User.builder()
                 .name("Yury")
                 .birthday(LocalDateFormatter.format("1987-05-26"))
@@ -45,20 +50,93 @@ class UserDaoTest extends IntegrationTestBase {
                 .build();
         userDao.save(user);
 
+        //When
         List<User> users = userDao.findAll();
 
+        //Then
         assertThat(users.size()).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("user with name=Vlad should be return")
     void findByEmailAndPassword() {
+
+        //Given
+        String email = "vlad@gmail.com";
+        String password = "456";
+
+        //When
+        User findUser = userDao.findByEmailAndPassword(email, password).orElse(null);
+
+
+        //Then
+        assertThat(findUser).isNotEqualTo(null);
+        assertThat(findUser.getName()).isEqualTo("Vlad");
     }
 
     @Test
+    @DisplayName("after deleting a user users size should be 4 and findUserById(1) should return null")
     void delete() {
+        //Given
+        userDao.delete(1);
+
+        //When
+        List<User> users = userDao.findAll();
+
+        //Then
+        assertThat(users.size()).isEqualTo(4);
+        assertThat(userDao.findById(1).orElse(null)).isEqualTo(null);
+
     }
 
-    @Test
-    void update() {
+    @Nested
+    @DisplayName("update method tests")
+    class UpdateTests {
+        @Test
+        @DisplayName("Update user with update() method")
+        void updateUser() {
+            //Given
+            User user = User.builder()
+                    .id(1)
+                    .name("Yury")
+                    .birthday(LocalDateFormatter.format("1987-05-26"))
+                    .gender(Gender.MALE)
+                    .password("12345")
+                    .role(Role.ADMIN)
+                    .email("zalupa@mail.ru")
+                    .build();
+            userDao.update(user);
+
+            //When
+            List<User> users = userDao.findAll();
+            User findUser = userDao.findById(1).orElse(null);
+
+            //Then
+            assertThat(users.size()).isEqualTo(5);
+            assertThat(findUser.getName()).isEqualTo("Yury");
+        }
+
+        @Test
+        @DisplayName("updating a user with a non-existent id")
+        void savingTheUserWhenUpdateCalling() {
+            //Given
+            User user = User.builder()
+                    .id(6)
+                    .name("Yury")
+                    .birthday(LocalDateFormatter.format("1987-05-26"))
+                    .gender(Gender.MALE)
+                    .password("12345")
+                    .role(Role.ADMIN)
+                    .email("zalupa@mail.ru")
+                    .build();
+            userDao.update(user);
+
+            //When
+            List<User> users = userDao.findAll();
+            User findUser = userDao.findById(6).orElse(null);
+
+            //Then
+            assertThat(findUser).isEqualTo(null);
+        }
     }
 }
